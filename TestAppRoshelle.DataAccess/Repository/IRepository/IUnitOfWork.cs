3b457f065de4ac4e969c1ba31e8a4c3b8a93df05
Roshelle <rozelsara@gmail.com>
2021-06-28 20:16:17 +0530

    Add project files.

---
+++ TestAppRoshelle.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using TestAppRoshelle.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestAppRoshelle.DataAccess.Repository.IRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ICategoryRepository Category { get; }
+        IProductRepository Product { get; }
+        ICompanyRepository Company { get; }
+        IApplicationUserRepository ApplicationUser { get; }
+        ISP_Call SP_Call { get; }
+
+        void Save();
+    }
+}
